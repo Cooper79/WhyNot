86a0e8d61eb1896f6b701b15dd13ab699f6e2223
Cooper79 <46046321+Cooper79@users.noreply.github.com>
2019-10-29 12:37:46 +0200

    Add files via upload
    Laba 1

---
+++ 1 task.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many hours ruuner will run?");
+            int hr = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What is acceleration of ruuner?");
+            int ac = Convert.ToInt32(Console.ReadLine());
+            int v = hr*hr*ac;
+            Console.WriteLine("{0} kilometers runner will run!", v);
+            
+            Console.ReadKey();
+        }
+    }
+}
