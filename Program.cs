6680f74d224fe5fd29eeab646cb9fcb3cc45cde7
Cooper79 <46046321+Cooper79@users.noreply.github.com>
2019-12-10 11:18:03 +0200

    Laba 11 Task 1

--- Program.cs
+++ Program.cs
@@ -4,234 +4,99 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Laba_9
+namespace Laba_11
 {
-    class Program
+    class Hospital
     {
-        enum Colors
+        public void MRT(Patient patient)
         {
-            White = 0,
-            Black,
-            Blue,
-            Orange,
-            Red,
-            Green
-        }
-        public abstract class Shape : IDraw
-        {
-            public double Area;
-            public double Perimeter;
-            public string Name { get; }
-            public ConsoleColor Color { get; set; }
-            public double Length { get; set; }
-            public abstract double area();
-            public abstract double perimeter();
-            public abstract void Draw();
-
-            public Shape(string name)
-            {
-                Random rand = new Random();
-                this.Name = name;
-                this.Length = rand.Next(0, 100);
-                int temp = rand.Next(0, 6);
-                var color = Enum.Parse(typeof(ConsoleColor), ((Colors)temp).ToString());
-                this.Color = (ConsoleColor)color;
-
-                Area = area();
-                Perimeter = perimeter();
-            }
-            public Shape(string name, double len)
-            {
-                Random rand = new Random();
-                this.Name = name;
-                this.Length = len;
-                int temp = rand.Next(0, 6);
-                var color = Enum.Parse(typeof(ConsoleColor), ((Colors)temp).ToString());
-                this.Color = (ConsoleColor)color;
-
-                Area = area();
-                Perimeter = perimeter();
-            }
-            public Shape(string name, double len, string _color)
-            {
-                Random rand = new Random();
-                this.Name = name;
-                this.Length = len;
-                var color = Enum.Parse(typeof(ConsoleColor), _color);
-                this.Color = (ConsoleColor)color;
-
-                Area = area();
-                Perimeter = perimeter();
-            }
+            patient.MRT = true;
         }
 
-        public class Square : Shape
+        public void KT(Patient patient)
         {
-            public Square(string name) : base(name) { }
-            public Square(string name, double len) : base(name, len) { }
-            public Square(string name, double len, string color) : base(name, len, color) { }
-            public override double area()
-            {
-                Area = Length * Length;
-                return Area;
-            }
-
-            public override void Draw()
-            {
-                Console.BackgroundColor = Color;
-                Console.WriteLine(Name);
-                Console.WriteLine(Length);
-                Console.WriteLine(Area);
-                Console.WriteLine(Perimeter);
-            }
-
-            public override double perimeter()
-            {
-                Perimeter = 4 * Length;
-                return Perimeter;
-            }
+            patient.KT = true;
         }
 
-        class Circle : Shape
+        public void Rentgen(Patient patient)
         {
-            public Circle(string name) : base(name) { }
-            public Circle(string name, double len) : base(name, len) { }
-            public Circle(string name, double len, string color) : base(name, len, color) { }
-            public override double area()
-            {
-                Area = Length * Length * Math.Sqrt(3) / 4;
-                return Area;
-            }
-            public override double perimeter()
-            {
-                Perimeter = 3 * Length;
-                return Perimeter;
-            }
-            public override void Draw()
-            {
-                Console.BackgroundColor = Color;
-                Console.WriteLine(Name);
-                Console.WriteLine(Length);
-                Console.WriteLine(Area);
-                Console.WriteLine(Perimeter);
-            }
+            patient.Rentgen = true;
         }
 
-        class Triangle : Shape
+        public void LOR(Patient patient)
         {
-            public Triangle(string name) : base(name) { }
-            public Triangle(string name, double len) : base(name, len) { }
-            public Triangle(string name, double len, string color) : base(name, len, color) { }
-            public override double area()
-            {
-                Area = Length * Length * Math.Sqrt(3) / 4;
-                return Area;
-            }
-            public override double perimeter()
-            {
-                Perimeter = 3 * Length;
-                return Perimeter;
-            }
-            public override void Draw()
-            {
-                Console.BackgroundColor = Color;
-                Console.WriteLine(Name);
-                Console.WriteLine(Length);
-                Console.WriteLine(Area);
-                Console.WriteLine(Perimeter);
-            }
+            patient.LOR = true;
         }
 
-        class Picture : IDraw
+        public void UZI(Patient patient)
         {
-            List<Shape> shapes;
-            int amount { get; set; }
-
-            public Picture()
-            {
-                shapes = new List<Shape>(0);
-            }
-            public Picture(int n)
-            {
-                shapes = new List<Shape>(n);
-            }
-
-            public Shape this[int index]
-            {
-                get
-                {
-                    if (index >= 0 && index < shapes.Count)
-                        return shapes[index];
-                    else
-                    {
-                        Console.WriteLine("Попытка обращения за пределы массива.");
-                        return null;
-                    }
-                }
-            }
-
-            public void AddShape(Shape shape)
-            {
-                shapes.Add(shape);
-            }
-
-            public void DeleteByName(string name)
-            {
-                shapes.Remove(shapes.Find(x => x.Name == name));
-            }
-
-            public void DeleteByType(string type)
-            {
-                type = "Laba 9." + type;
-                shapes.Remove(shapes.Find(x => x.GetType().ToString() == type));
-            }
-
-            public void DeleteByLength(string type)
-            {
-                shapes.RemoveAll(x => x.Area > 100);
-            }
-            void IDraw.Draw()
-            {
-                for (int i = 0; i < shapes.Count; i++)
-                {
-                    shapes[i].Draw();
-                }
-            }
+            patient.UZI = true;
         }
 
-        interface IDraw
+        public void R_Test(Patient patient)
         {
-            void Draw();
+            patient.R_Test = true;
         }
 
-        class Painter
-        {
-            public void DrawShape(IDraw shape)
-            {
-                shape.Draw();
-            }
-            public void DrawShapes(List<IDraw> shapes)
-            {
-                foreach (Shape shape in shapes)
-                {
-                    shape.Draw();
-                }
-            }
-        }
-        static void Main(string[] args)
-        {
-            Square s = new Square("Квадрат");
-            Console.WriteLine($"Периметр {s.Name}а = {s.Perimeter}");
-            Console.WriteLine($"Площадь {s.Name}а = {s.Area}");
-            Console.WriteLine($"Цвет {s.Name}а = {s.Color}");
-            Console.WriteLine();
+    }
 
-            Picture picture = new Picture();
-            picture.AddShape(s);
-            picture.DeleteByType("Square");
+    class Patient
+    {
+        public bool MRT { get; set; }
 
-            Console.ReadLine();
+        public bool KT { get; set; }
+
+        public bool Rentgen { get; set; }
+
+        public bool LOR { get; set; }
+
+        public bool UZI { get; set; }
+
+        public bool R_Test { get; set; }
+
+        public void OUT()
+        {
+            Console.WriteLine($"MRT: {(MRT ? "done" : "undone")}");
+
+            Console.WriteLine($"KT: {(KT ? "done" : "undone")}");
+
+            Console.WriteLine($"Rentgen: {(Rentgen ? "done" : "undone")}");
+
+            Console.WriteLine($"LOR: {(LOR ? "done" : "undone")}");
+
+            Console.WriteLine($"UZI: {(UZI ? "done" : "undone")}");
+
+            Console.WriteLine($"R_test: {(R_Test ? "done" : "undone")}");
         }
     }
 
+    class Program
+    {
+        public delegate void Delegate(Patient patient);
+        
+        static void Main(string[] args)
+        {
+            Hospital hospital = new Hospital();
+            Patient patient = new Patient();
+
+            Delegate Analises = null;
+            Delegate MRT = new Delegate(hospital.MRT);
+            Analises += MRT;
+            Delegate KT = new Delegate(hospital.KT);
+            Analises += KT;
+            Delegate Rentgen = new Delegate(hospital.Rentgen);
+            Analises += Rentgen;
+            Delegate LOR = new Delegate(hospital.LOR);
+            Analises += LOR;
+            Delegate UZI = new Delegate(hospital.UZI);
+            Analises += UZI;
+            Delegate R_Test = new Delegate(hospital.R_Test);
+            Analises += R_Test;
+
+            Analises(patient);
+
+            patient.OUT();
+            Console.ReadKey();
+        }
+    }
 }
