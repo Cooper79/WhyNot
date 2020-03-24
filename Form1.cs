e59a81a072709a2f4a69a39d0f8e89cb865faa42
Cooper79 <46046321+Cooper79@users.noreply.github.com>
2020-03-24 19:33:27 +0200

    You can`t press OK

--- Form1.cs
+++ Form1.cs
@@ -8,234 +8,169 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Calculator
+namespace Press_OK
 {
     public partial class Form1 : Form
     {
-        double a, b;
-        int count;
-        string k;
-        double otvet;
+        private int ticks=1;
+        private bool show = false;
+        private bool isClickable = false;
 
         public Form1()
         {
-            
             InitializeComponent();
+            timer1.Start();
         }
 
-        
-
-        private void TextBox1_TextChanged(object sender, EventArgs e)
+        private void Button2_MouseMove(object sender, MouseEventArgs e)
         {
+            Point cur = this.PointToClient(Cursor.Position);
+            Point loc = button2.Location;
+            int x_new = loc.X;
+            int y_new = loc.Y;
 
+            if (this.isClickable)
+                return;
+
+            if (cur.X < loc.X + 10) //движение вправо
+            {
+                x_new += 30;
+                button2.Width = button2.Width - 2;
+                button2.Height = button2.Height - 1;
+            }
+
+            if(cur.X +10>loc.X + button2.Width) //движение влево
+            {
+                x_new += -30;
+                button2.Width = button2.Width - 2;
+                button2.Height = button2.Height - 1;
+            }
+
+            if (x_new < 30) //отступ слева
+            {
+                x_new = 30;
+                button2.Width = button2.Width - 2;
+                button2.Height = button2.Height - 1;
+            }
+
+            if(x_new>this.Width - button2.Width - 30) //отступ справа
+            {
+                x_new = this.Width - button2.Width - 30;
+                button2.Width = button2.Width - 2;
+                button2.Height = button2.Height - 1;
+            }
+
+            if (cur.Y < loc.Y + 10) //движение вниз
+            {
+                y_new += 30;
+                button2.Width = button2.Width - 2;
+                button2.Height = button2.Height - 1;
+            }
+
+            if(cur.Y + 10 > loc.Y + button2.Height) //движение вверх
+            {
+                y_new += -30;
+                button2.Width = button2.Width - 2;
+                button2.Height = button2.Height - 1;
+            }
+
+            if (y_new < 30) //отступ сверху
+            {
+                y_new = 10;
+                button2.Width = button2.Width - 2;
+                button2.Height = button2.Height - 1;
+            }
+
+            if(y_new>this.Height - button2.Height - 30) //отступ снизу
+            {
+                y_new = this.Height - button2.Height - 45;
+                button2.Width = button2.Width - 2;
+                button2.Height = button2.Height - 1;
+            }
+
+            if (y_new > this.Height - button2.Height - 15) //курсор внизу формы
+            {
+                y_new = this.Height - button2.Height - 80;
+                button2.Width = button2.Width - 2;
+                button2.Height = button2.Height - 1;
+            }
+
+            if(x_new>this.Width - button2.Width - 15) //курсор справа формы
+            {
+                x_new += -100;
+                button2.Width = button2.Width - 2;
+                button2.Height = button2.Height - 1;
+            }
+
+            if (y_new < 15) //курсор вверху формы
+            {
+                y_new += 50;
+                button2.Width = button2.Width - 2;
+                button2.Height = button2.Height - 1;
+            }
+
+            if (x_new < 15) //курсор слева формы
+            {
+                x_new += 100;
+                button2.Width = button2.Width - 2;
+                button2.Height = button2.Height - 1;
+            }
+
+            button2.Location=new Point(x_new, y_new);
         }
-        private void Button5_Click(object sender, EventArgs e)
+        private void Form(object sender, EventArgs e)
         {
-            if (textBox1.Text == "0" && textBox1.Text != null)
-            {
-                textBox1.Text = "1";
-            }
-            else
-            {
-                textBox1.Text = textBox1.Text + "1";
-            }
-        }
-
-        private void Button8_Click(object sender, EventArgs e)
-        {
-            if (textBox1.Text == "0" && textBox1.Text != null)
-            {
-                textBox1.Text = "2";
-            }
-            else
-            {
-                textBox1.Text = textBox1.Text + "2";
-            }
-        }
-
-        private void Button7_Click(object sender, EventArgs e)
-        {
-            if (textBox1.Text == "0" && textBox1.Text != null)
-            {
-                textBox1.Text = "3";
-            }
-            else
-            {
-                textBox1.Text = textBox1.Text + "3";
-            }
-        }
-
-        private void Button10_Click_1(object sender, EventArgs e)
-        {
-            if (textBox1.Text == "0" && textBox1.Text != null)
-            {
-                textBox1.Text = "4";
-            }
-            else
-            {
-                textBox1.Text = textBox1.Text + "4";
-            }
-        }
-
-        private void Button6_Click_1(object sender, EventArgs e)
-        {
-            if (textBox1.Text == "0" && textBox1.Text != null)
-            {
-                textBox1.Text = "5";
-            }
-            else
-            {
-                textBox1.Text = textBox1.Text + "5";
-            }
-        }
-
-        private void Button9_Click(object sender, EventArgs e)
-        {
-            if (textBox1.Text == "0" && textBox1.Text != null)
-            {
-                textBox1.Text = "6";
-            }
-            else
-            {
-                textBox1.Text = textBox1.Text + "6";
-            }
-        }
-
-        private void Button13_Click(object sender, EventArgs e)
-        {
-            if (textBox1.Text == "0" && textBox1.Text != null)
-            {
-                textBox1.Text = "7";
-            }
-            else
-            {
-                textBox1.Text = textBox1.Text + "7";
-            }
-        }
-
-        private void Button11_Click(object sender, EventArgs e)
-        {
-            if (textBox1.Text == "0" && textBox1.Text != null)
-            {
-                textBox1.Text = "8";
-            }
-            else
-            {
-                textBox1.Text = textBox1.Text + "8";
-            }
-        }
-
-        private void Button12_Click(object sender, EventArgs e)
-        {
-            if (textBox1.Text == "0" && textBox1.Text != null)
-            {
-                textBox1.Text = "9";
-            }
-            else
-            {
-                textBox1.Text = textBox1.Text + "9";
-            }
-        }
-
-        private void Button14_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = textBox1.Text + "0";
-        }
-
-        private void Button1_Click(object sender, EventArgs e) // "+"
-        {
-            a = Convert.ToDouble(textBox1.Text);
-            textBox1.Text = "";
-            count = 1;
-        }
-
-        private void Button2_Click(object sender, EventArgs e)// "-"
-        {
-            a = Convert.ToDouble(textBox1.Text);
-            textBox1.Text = "";
-            count = 2;
-        }
-
-        private void Button4_Click(object sender, EventArgs e) // "*"
-        {
-            a = float.Parse(textBox1.Text);
-            textBox1.Text = "";
-            count = 3;
-
-        }
-
-        private void Button3_Click(object sender, EventArgs e) // "/"
-        {
-            a = float.Parse(textBox1.Text);
-            textBox1.Text = "";
-            count = 4;
-
-        }
-
-        private void Button17_Click(object sender, EventArgs e)  // ","
-        {
-            textBox1.Text += ",";
-        }
-
-        private void Button15_Click(object sender, EventArgs e) // BACK
-        {
-            int lenght = textBox1.Text.Length - 1;
-            string text = textBox1.Text;
-            textBox1.Clear();
-            for (int i = 0; i < lenght; i++)
-            {
-                textBox1.Text = textBox1.Text + text[i];
-            }
-        }
-
-        private void Button16_Click(object sender, EventArgs e)  //result
-        {
-            double SecondNumber;
-            double Result;
-            string res;
-
-            SecondNumber = Convert.ToDouble(textBox1.Text);
             
-            if (count == 1)
-            {
-                Result = a + SecondNumber;
-                res = Convert.ToString(Result);
-                textBox1.Text = res;
-            }
-            if (count == 2)
-            {
-                Result = a - SecondNumber;
-                res = Convert.ToString(Result);
-                textBox1.Text = res;
-                
-            }
-            if (count == 3)
-            {
-                Result = (a * SecondNumber);
-                textBox1.Text = Convert.ToString(Result);
-                a = Result;
-            }
-            if (count == 4)
-            {
-                if (SecondNumber == 0)
-                {
-                    textBox1.Text = "Cannot divide by zero";
+        }
 
+        private void Cancel(object sender, EventArgs e)
+        {
+            this.Close();
+            button1.TabStop = false;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            button2.TabStop = false;
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            
+            if (this.show)
+            {
+                this.Text = "Press 'OK' button";
+                this.show = false;
+            }
+            else
+            {
+                this.Text = "";
+                this.show = true;
+            }
+            ++ticks;
+
+            if (this.ticks <= 16)
+                return;
+
+            this.timer1.Enabled = false;
+            this.show = false;
+        }
+
+        private void Timer2_Tick(object sender, EventArgs e)
+        {
+            
+                if (this.show)
+                {
+                    this.Text = "'OK' will never been pressed!!!";
+                    this.show = false;
                 }
                 else
                 {
-                    Result = (a / SecondNumber);
-                    textBox1.Text = Convert.ToString(Result);
-                    a = Result;
+                    this.Text = "";
+                    this.show = true;
                 }
-            }
-
-        }
-
-        private void Button18_Click(object sender, EventArgs e) // Clear All
-        {
-            textBox1.Text = "";
+                ++this.ticks;
+            
         }
     }
-    
 }
